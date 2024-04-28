4e0a7db26db2ecdc18cb5e7b68d1296494e25fc1
OyuNet <oyunarda662@gmail.com>
2024-04-28 18:07:46 +0300

    CORS added.

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ import { randomString } from "./functions/randomString.js";
 const app = express();
 config();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get("/", async (req, res) => {
 
     const totalLinks = await getLinkAmount();
